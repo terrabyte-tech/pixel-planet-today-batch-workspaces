472d6ecc6c0b09c2133c71543d133a0bc2465441
sharper <hi@sammyharper.com>
2022-12-07 11:36:26 -0600

    added commands for opening webpages in their respective Edge profile

--- new-post.bat
+++ new-post.bat
@@ -1,14 +1,20 @@
 @ECHO OFF
 
 @REM Profile 1 is Pixel; Profile 2 is GHM; Profile 3 is Terrabyte
+@REM ____________________________________________________________
 
-@REM Pixel Planet Today
-start microsoft-edge:"https://www.instagram.com/pixelplanettoday/" --profile-directory="Profile 1"
-start microsoft-edge:"https://www.facebook.com/pixelplanettoday/?ref=pages_you_manage" --profile-directory="Profile 1"
-start microsoft-edge:"https://business.facebook.com/latest/content_calendar?asset_id=106945221034132&nav_ref=page_composer_interruption&business_id=426922484643463" --profile-directory="Profile 1"
-start microsoft-edge:"https://app.wedonthavetime.org/" --profile-directory="Profile 1"
-start microsoft-edge:"https://drive.google.com/drive/folders/13WihWMy9Rm658CKBLY3l1qfo_Ck3-7Ds" --profile-directory="Profile 1"
+@REM Pixel Planet Today Pages
+start "Edge" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --profile-directory="Profile 1"
 
-@REM Terrabyte
-start microsoft-edge:"https://www.twitter.com/TerrabyteEco" --profile-directory="Profile 3"
-start microsoft-edge:"https://mastodon.eco" --profile-directory="Profile 3"
+start "Facebook Creator Studio" "https://business.facebook.com/latest/content_calendar?asset_id=106945221034132&nav_ref=page_composer_interruption&business_id=426922484643463"
+start "Instagram" "https://www.instagram.com/pixelplanettoday/"
+start "Facebook" "https://www.facebook.com/pixelplanettoday/?ref=pages_you_manage"
+start "We Don't Have Time" "https://app.wedonthavetime.org/"
+start "Drive > Carbonii" "https://drive.google.com/drive/folders/13WihWMy9Rm658CKBLY3l1qfo_Ck3-7Ds"
+
+
+@REM Terrabyte Pages
+start "Edge" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --profile-directory="Profile 3"
+
+start "Twitter" "https://www.twitter.com/TerrabyteEco"
+start "Mastodon" "https://mastodon.eco"
