c20978eae890369788d2d27c984307cfff59292a
sharper <hi@sammyharper.com>
2022-12-07 11:47:28 -0600

    added comments to new post workflow; created the new blog post workflow batch

--- new-blog-post.bat
+++ new-blog-post.bat
@@ -1,1 +1,15 @@
 @ECHO OFF
+
+@REM Profile 1 is Pixel; Profile 2 is GHM; Profile 3 is Terrabyte
+@REM ____________________________________________________________
+
+@REM Pixel Planet Today Pages
+start "Edge" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --profile-directory="Profile 1"
+echo starting Edge in Profile 1 (Pixel Planet Today)...
+
+start "Pixel Planet Today Blog: Admin" "https://blog.pixelplanettoday.com/wp-admin"
+echo opening Pixel Planet Today Blog: Admin
+start "Pixel Planet Today Blog" "https://blog.pixelplanettoday.com"
+echo opening Pixel Planet Today Blog
+
+echo closing batch
--- new-post.bat
+++ new-post.bat
@@ -5,16 +5,27 @@
 
 @REM Pixel Planet Today Pages
 start "Edge" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --profile-directory="Profile 1"
+echo starting Edge in Profile 1 (Pixel Planet Today)...
 
-start "Facebook Creator Studio" "https://business.facebook.com/latest/content_calendar?asset_id=106945221034132&nav_ref=page_composer_interruption&business_id=426922484643463"
+start "Meta Creator Studio" "https://business.facebook.com/latest/content_calendar?asset_id=106945221034132&nav_ref=page_composer_interruption&business_id=426922484643463"
+echo opening Meta Creator Studio
 start "Instagram" "https://www.instagram.com/pixelplanettoday/"
+echo opening Instagram
 start "Facebook" "https://www.facebook.com/pixelplanettoday/?ref=pages_you_manage"
+echo opening Facebook
 start "We Don't Have Time" "https://app.wedonthavetime.org/"
-start "Drive > Carbonii" "https://drive.google.com/drive/folders/13WihWMy9Rm658CKBLY3l1qfo_Ck3-7Ds"
+echo opening We Don't Have Time
+start "Google Drive > Carbonii" "https://drive.google.com/drive/folders/13WihWMy9Rm658CKBLY3l1qfo_Ck3-7Ds"
+echo opening Google Drive > Carbonii
 
 
 @REM Terrabyte Pages
 start "Edge" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --profile-directory="Profile 3"
+echo starting Edge in Profile 3 (Terrabyte)...
 
 start "Twitter" "https://www.twitter.com/TerrabyteEco"
+echo opening Twitter
 start "Mastodon" "https://mastodon.eco"
+echo opening Mastodon
+
+echo closing batch
