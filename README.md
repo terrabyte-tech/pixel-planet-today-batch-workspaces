6a32d6acc440729a0b770ff43054505b2d475285
sharper <hi@sammyharper.com>
2022-12-07 11:10:47 -0600

    added batch files for New Post and New Blog Post workflows

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Easily open a series of webpages that are needed for various Pixel Planet Today 
 
 Batch files and what they do:
 
-- `-new-post` opens websites used to post on social media about a new Pixel Planet Today post
-- `-new-blog-post` opens websites used to post on the Pixel Planet Today blog
+- `new-post.bat` opens websites used to post on social media about a new Pixel Planet Today post
+- `new-blog-post.bat` opens websites used to post on the Pixel Planet Today blog
 
 **Note:** These configurations align with the setup of Sammy's machine. In order to create one for yourself, create your own branch and make adjustments accordingly.
---
+++ new-post.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+@REM Profile 1 is Pixel; Profile 2 is GHM; Profile 3 is Terrabyte
+
+@REM Pixel Planet Today
+start microsoft-edge:"https://www.instagram.com/pixelplanettoday/" --profile-directory="Profile 1"
+start microsoft-edge:"https://www.facebook.com/pixelplanettoday/?ref=pages_you_manage" --profile-directory="Profile 1"
+start microsoft-edge:"https://business.facebook.com/latest/content_calendar?asset_id=106945221034132&nav_ref=page_composer_interruption&business_id=426922484643463" --profile-directory="Profile 1"
+start microsoft-edge:"https://app.wedonthavetime.org/" --profile-directory="Profile 1"
+start microsoft-edge:"https://drive.google.com/drive/folders/13WihWMy9Rm658CKBLY3l1qfo_Ck3-7Ds" --profile-directory="Profile 1"
+
+@REM Terrabyte
+start microsoft-edge:"https://www.twitter.com/TerrabyteEco" --profile-directory="Profile 3"
+start microsoft-edge:"https://mastodon.eco" --profile-directory="Profile 3"
