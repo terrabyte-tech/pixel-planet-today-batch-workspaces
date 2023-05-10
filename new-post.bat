610d7063f7fe2b1cc990a2787d888611859b9f33
sharper <hi@sammyharper.com>
2023-05-10 11:34:40 -0500

    feat: make all routes options on a main menu
    allow for any action to be done in any order, or multiple times if necessary. Only exists when the user selects to

--- new-post.bat
+++ new-post.bat
@@ -3,6 +3,25 @@
 @REM Profile 1 is Pixel; Profile 3 is Terrabyte
 @REM ____________________________________________________________
 
+:mainMenu
+cls
+echo ==========
+echo Choose from the following actions.
+echo ==========
+echo.
+echo 1: Open Pixel Planet Today pages
+echo 2: Open Terrabyte pages
+echo 3: Open File Explorer
+echo 4: EXIT
+
+set /p menuInput=Enter a number from above, then press ENTER:
+if %menuInput%==1 goto openPixelPages
+if %menuInput%==2 goto openTbPages
+if %menuInput%==3 goto openExplorer
+if %menuInput%==4 goto exit
+
+
+:openPixelPages
 @REM Pixel Planet Today Pages
 start "Edge" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --profile-directory="Profile 1"
 echo starting Edge in Profile 1 (Pixel Planet Today)...
@@ -18,32 +37,11 @@ echo opening We Don't Have Time
 start "Google Drive > Carbonii" "https://drive.google.com/drive/folders/13WihWMy9Rm658CKBLY3l1qfo_Ck3-7Ds"
 echo opening Google Drive (Carbonii)
 
-
-choice /c yn /n /m "Open File Explorer? (y/n)"
-set INPUT1=%ERRORLEVEL%
-if %INPUT1% EQU 1 goto yesExplorer
-if %INPUT1% EQU 2 goto noExplorer
-
-:yesExplorer
-if not exist "%~dp0%file-paths\ppt-days.txt" goto noFolder
-
-set /p pptfilepath=<%~dp0%file-paths\ppt-days.txt
-
-echo opening in File Explorer...
-%SystemRoot%\explorer.exe %pptfilepath%
-goto noExplorer
-
-:noFolder
-echo unable to open File Explorer; folder not found
-
-:noExplorer
-choice /c yn /n /m "Share on Terrabyte pages as well? (y/n)"
-set INPUT=%ERRORLEVEL%
-if %INPUT% EQU 1 goto yesTB
-if %INPUT% EQU 2 goto noTB
+start %~dp0%new-post_open.bat
+exit
 
 
-:yesTB
+:openTbPages
 @REM Terrabyte Pages
 start "Edge" "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --profile-directory="Profile 3"
 echo starting Edge in Profile 3 (Terrabyte)...
@@ -53,6 +51,26 @@ echo opening Twitter
 start "Mastodon" "https://mastodon.eco"
 echo opening Mastodon
 
-:noTB
+start %~dp0%new-post_open.bat
+exit
+
+
+:openExplorer
+if not exist "%~dp0%file-paths\ppt-days.txt" goto noFolder
+set /p pptfilepath=<%~dp0%file-paths\ppt-days.txt
+
+echo opening in File Explorer...
+%SystemRoot%\explorer.exe %pptfilepath%
+
+start %~dp0%new-post_open.bat
+exit
+
+:noFolder
+echo unable to open File Explorer; folder not found
+goto mainMenu
+
+
+:exit
 echo closing batch
-timeout /t 3 /nobreak
+timeout /t 1 /nobreak
+exit
